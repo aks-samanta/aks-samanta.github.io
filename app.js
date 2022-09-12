464c85c5b38a6f60e1aff7a717f9475fbdcad4c7
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2022-09-12 21:44:58 +0530

    another version

--- app.js
+++ app.js
@@ -14,7 +14,30 @@ function PageTransitions(){
             this.className += " active-btn";
         })
     }
+   
 }
 
-
 PageTransitions();
+
+allSections.forEach(addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+    if(id){
+        //remove selected from the other button
+        sectBtns.forEach((btn)=>{
+            btn.classList.remove("active");
+        })
+        e.target.classList.add("active");
+
+        //hide other sections
+
+        sections.forEach((section) => {
+            section.classList.remove("active");
+
+        })
+
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+}))
+
+
