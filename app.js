fdfdae694fddde0e9749a78a874c81c31f14fdb8
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2022-12-05 22:38:35 +0530

    resume download + open

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 document.getElementById("dual_fn_res").addEventListener("click", dual_fn);
 function dual_fn() {
-    window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
+    window.open(
+        'https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing',
+        '_blank' // <- This is what makes it open in a new window.
+      );
+      
+    // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
 }
 
 function PageTransitions(){
