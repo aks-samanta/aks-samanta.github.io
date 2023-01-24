9435cc95041ea888a62c35fdfff60f548970466a
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2023-01-24 12:12:03 +0530

    resume on other page added

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ function dual_fn() {
     // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
 }
 
+document.getElementById("dual_fn_res2").addEventListener("click", dual_fn);
+function dual_fn() {
+    window.open(
+        'https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing',
+        '_blank' // <- This is what makes it open in a new window.
+      );
+      
+    // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
+}
+
 function PageTransitions(){
     //Button click active class
 
