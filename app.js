a242cc4c30b380c987ea15f28e0850b47c9b248a
Akash Samanta <101733074+aks-samanta@users.noreply.github.com>
2023-01-24 14:56:25 +0530

    resume download and open rectified

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const sections = document.querySelectorAll(".section");
 const sectBtns = document.querySelectorAll(".controls");
 const sectBtn = document.querySelectorAll(".control");
 const allSections = document.querySelectorAll(".main-content");
-document.getElementById("dual_fn_res").addEventListener("click", dual_fn);
-function dual_fn() {
-  window.open(
-    "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing",
-    "_blank" // <- This is what makes it open in a new window.
-  );
+// document.getElementById("dual_fn_res").addEventListener("click", dual_fn);
+// function dual_fn() {
+//   window.open(
+//     "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing",
+//     "_blank" // <- This is what makes it open in a new window.
+//   );
 
-  // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
-}
+//   // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
+// }
 
-document.getElementById("dual_fn_res2").addEventListener("click", dual_fn);
-function dual_fn() {
-  window.open(
-    "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing",
-    "_blank" // <- This is what makes it open in a new window.
-  );
+// document.getElementById("dual_fn_res2").addEventListener("click", dual_fn);
+// function dual_fn() {
+//   window.open(
+//     "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing",
+//     "_blank" // <- This is what makes it open in a new window.
+//   );
 
-  // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
-}
+//   // window.location.href = "https://drive.google.com/file/d/1Y8Mof5D-2E3QAmG0Gd3En6l_rySZUmhD/view?usp=sharing";
+// }
 
 function PageTransitions() {
   //Button click active class
